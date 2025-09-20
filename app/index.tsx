2f4eb66806b896130a945adfb85581efc56adb12
RodrigoFFLima <rodrigofflima@hotmail.com>
2025-09-20 07:54:28 -0400

    feat: creating styles and refactor index

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,14 @@
-import { Text, View } from "react-native";
+import { styles } from "@/styles";
+import { TextInput, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Buscar"
+        placeholderTextColor="#666"
+      />
     </View>
   );
 }
---
+++ styles/index.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 16,
+    backgroundColor: "#E4E4E4",
+    alignItems: "center",
+  },
+  input: {
+    height: 50,
+    width: "95%",
+    paddingHorizontal: 8,
+    backgroundColor: "white",
+    borderColor: "#333",
+    borderWidth: 1,
+    borderRadius: 8,
+    marginHorizontal: 8,
+  },
+});
